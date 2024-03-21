41e580fb175dc87b498b866b56e7ad80f6bc55a4
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2024-03-21 14:27:16 +0000

    first commit

---
+++ exhibition-curation-fe/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState('');
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+// console.log(query)
+
+  const naturalHistoryUrl =
+    `https://data.nhm.ac.uk/api/3/action/package_search?q=${query}`;
+
+
+    const fetchNaturalHist = async () => {
+      const result = await fetch(naturalHistoryUrl)
+      result.json().then(jsonResponse => {
+        console.log(jsonResponse);
+        setResults(jsonResponse.result)
+      })
+    }
+
+    const handleInput = (e) => {
+      setInput(e.target.value);
+    };
+
+  const handleSearch = (e) => {
+    setQuery(input)
+    fetchNaturalHist();
+    }
+  return (
+    <>
+      <h1>Artwork & artifacts online</h1>
+      <div className="card">
+        <h2>Explore museum and gallery collections</h2>
+        <p>
+          Results are sourced from ~~~ gallery, ~~~ university and London
+          Natural History Museum collections
+        </p>
+      </div>
+      <div>
+        <h3>Input search criteria</h3>
+        <input onChange={handleInput}></input>
+        <button onClick={handleSearch}>Search collections</button>
+        {query === "" ? (
+          <></>
+        ) : results.count && results.count != 0 ? (
+          <>
+            <p>
+              {" "}
+              <em>{results.count} results found!</em>
+            </p>
+          </>
+        ) : (
+          <>
+            <p>
+              <em>No results currently archived about: {query}</em>
+            </p>
+          </>
+        )}
+       
+      </div>
+    </>
+  );
+}
+
+export default App
