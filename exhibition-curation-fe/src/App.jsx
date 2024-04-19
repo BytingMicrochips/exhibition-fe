145fcfe37515856d88a6b7bdaa93ffc48fbe9990
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2024-04-19 16:01:36 +0100

    add fetchResults function sets modal to false

--- exhibition-curation-fe/src/App.jsx
+++ exhibition-curation-fe/src/App.jsx
@@ -39,6 +39,7 @@ function App() {
 
   const fetchResults = async () => {
     const emptyMet = [];
+    setModal(false);
     setIsLoading(true);
     const fullRequest = `${apiSelector}${input}`;
 
